025f6dcb8df94e2d25b1366ad673a3233f64cf7b
dragonH <10220jack@gmail.com>
2023-01-07 19:16:37 +0800

    modify payload

--- personal_reason/libs/_request.py
+++ personal_reason/libs/_request.py
@@ -7,27 +7,30 @@ import html
 def _format_payload(
     _payload: str,
     _action: str,
-    _session_id: str
+    _session_id: str,
+    _payload_type: str
 ):
     """
         This function is to format payload
     """
     payload_formated = f"""
-<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
-    <soap12:Body>
-        <SystemObjectRun xmlns="http://scsservices.net/">
-        <args>
-            <SessionGuid>{_session_id}</SessionGuid>
-            <Action>{_action}</Action>
-            <Format>Xml</Format>
-            <Bytes />
-            <Value>{html.escape(_payload.strip())}
-            </Value>
-        </args>
-    </SystemObjectRun>
-</soap12:Body>
-</soap12:Envelope>
-"""
+    <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
+        <soap12:Body>
+            <{'SystemObjectRun' if _payload_type == 'login' else 'BusinessObjectRun'} xmlns="http://scsservices.net/">
+            <args>
+                <SessionGuid>{_session_id}</SessionGuid>
+                <Action>{_action}</Action>
+                {'<ProgID>WATT0022000</ProgID>' if _payload_type != 'login' else None}
+                <Format>Xml</Format>
+                <Bytes />
+                <Value>{html.escape(_payload.strip())}
+                </Value>
+            </args>
+        </{'SystemObjectRun' if _payload_type == 'login' else 'BusinessObjectRun'}>
+    </soap12:Body>
+    </soap12:Envelope>
+    """
+    print(payload_formated)
     return payload_formated
 
 def send_request(
@@ -35,7 +38,8 @@ def send_request(
     _request_type: str,
     _action: str,
     _payload: str,
-    _session_id: str
+    _session_id: str,
+    _payload_type: str
 ):
     """
         This function is to send requet
@@ -46,7 +50,8 @@ def send_request(
     payload_format = _format_payload(
         _payload=_payload,
         _action=_action,
-        _session_id=_session_id
+        _session_id=_session_id,
+        _payload_type=_payload_type
     )
     if _request_type == 'GET':
         response = requests.get(

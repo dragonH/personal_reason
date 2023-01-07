498f8df1e611a156415c9bf9d47a1352cdbd1929
dragonH <10220jack@gmail.com>
2023-01-07 19:41:59 +0800

    add 'main_logger' & 'request_logger'

--- personal_reason/libs/__init__.py
+++ personal_reason/libs/__init__.py
@@ -2,7 +2,10 @@
     This script is to initialize libs
 """
 from ._request import send_request
+from ._logger import main_logger, request_logger
 
 __all__ = [
-    'send_request'
+    'send_request',
+    'main_logger',
+    'request_logger'
 ]
